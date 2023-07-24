b92eff9332f62a3223f42cc6e7f1f4b874978241
Zack <lorenx@163.com>
2023-07-24 09:02:32 +0800

    feat: update models

--- app/constant.ts
+++ app/constant.ts
@@ -74,19 +74,19 @@ export const DEFAULT_MODELS = [
   },
   {
     name: "gpt-4-0613",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-32k",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-32k-0314",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-4-32k-0613",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-3.5-turbo",
@@ -98,7 +98,7 @@ export const DEFAULT_MODELS = [
   },
   {
     name: "gpt-3.5-turbo-0613",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-3.5-turbo-16k",
@@ -106,6 +106,6 @@ export const DEFAULT_MODELS = [
   },
   {
     name: "gpt-3.5-turbo-16k-0613",
-    available: true,
+    available: false,
   },
 ] as const;
